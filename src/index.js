e7c70d46385251aa320147f824fa4f3595e49ead
Andrew Faryniarz <afaryniarz3@gmail.com>
2023-10-04 16:04:29 -0400

    commit tree a2771e811ffa2a8d2700a0062cdbcf7d508f44f0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+// const toDoInput = document.getElementById('create-task-form')
+// console.log(toDoInput);
+// toDoInput.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   const descriptionInput = document.getElementById('new-task-description')
+//   const taskArea = document.getElementById('tasks')
+//   const newTaskItem = document.createElement('li')
+//   const addNewTaskItem = taskArea.appendChild(newTaskItem)
+
+//   addNewTaskItem.textContent = descriptionInput.value
+
+//   toDoInput.reset()
+// })
+
+// });
+
+const toDoInput = document.getElementById('create-task-form')
+toDoInput.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+    const descriptionInput = document.getElementById('new-task-description')
+    const taskArea = document.getElementById('tasks')
+    const newTaskItem = document.createElement('li')
+    const addNewTaskItem = taskArea.appendChild(newTaskItem)
+
+    addNewTaskItem.textContent = descriptionInput.value
+    toDoInput.reset()
+})
+
 });
